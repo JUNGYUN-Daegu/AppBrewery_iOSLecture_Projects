2370b16fbf7dda2cb1c24145528df8e9274d5673
JO JUNGYUN <66199062+JUNGYUN-Daegu@users.noreply.github.com>
2020-09-04 18:54:24 +0900

    I Am Rich

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by 조중윤 on 2020/07/07.
+//  Copyright © 2020 Jojung-yun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
