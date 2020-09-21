1568a73c767bd22bea00e96c60aee1174bdbdc88
JO JUNGYUN <66199062+JUNGYUN-Daegu@users.noreply.github.com>
2020-09-21 22:15:35 +0900

    Update Data with Core Data (Update in CRUD)

--- Todoey-iOS13/Todoey/Controller/TodoListViewController.swift
+++ Todoey-iOS13/Todoey/Controller/TodoListViewController.swift
@@ -42,8 +42,11 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
+    //MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(tableView.cellForRow(at: indexPath)?.textLabel?.text ?? "")
+        
+//        itemArray[indexPath.row].setValue("Completed", forKey: "title")
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
