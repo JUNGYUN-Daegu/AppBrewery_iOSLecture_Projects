6c10aa7bb10646ee2c2665a4b4c1b06b83f33157
JO JUNGYUN <66199062+JUNGYUN-Daegu@users.noreply.github.com>
2020-12-29 22:15:36 +0900

    Add files via upload
    Computed property
    Basic Understanding of getter and setter

---
+++ ComputedProperty/ComputedProperty/main.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  ComputedProperty
+//
+//  Created by 조중윤 on 2020/12/29.
+//
+
+import Foundation
+
+let pizzaInInches: Int = 16
+var numberOfPeople: Int = 12
+let slicesPerPerson: Int = 4
+
+var numberOfSlices: Int {
+    get {
+        return pizzaInInches - 4
+    }
+}
+
+var numberOfPizza: Int {
+    get {
+        let numberOfPeopleFedPerPizza = numberOfSlices / slicesPerPerson
+        return numberOfPeople / numberOfPeopleFedPerPizza
+    }
+    
+    set {
+        let totalSlices = numberOfSlices * newValue
+        numberOfPeople = totalSlices / slicesPerPerson
+    }
+}
+
+numberOfPizza = 4
+print(numberOfPeople)
